9587c9f2f49a444c1a0c050e0f8851d703a0c26b
patrykkondrat <01150244@pw.edu.pl>
2023-04-22 08:19:40 +0200

    Initial commit

---
+++ backend/src/main/java/org/patrykkondrat/countries/CountriesApp.java
@@ -0,0 +1,13 @@
+package org.patrykkondrat.countries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountriesApp.class, args);
+    }
+
+}
