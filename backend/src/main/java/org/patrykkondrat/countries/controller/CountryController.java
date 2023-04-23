8e59ee3707dfdfaf9e61952da25e96748de4c352
patrykkondrat <01150244@pw.edu.pl>
2023-04-23 13:10:11 +0200

    GraphQL lib had to send the post method, I don't use it, so the get method is more accurate

--- backend/src/main/java/org/patrykkondrat/countries/controller/CountryController.java
+++ backend/src/main/java/org/patrykkondrat/countries/controller/CountryController.java
@@ -1,14 +1,11 @@
 package org.patrykkondrat.countries.controller;
 
-import org.patrykkondrat.countries.client.GraphQLClient;
-import org.patrykkondrat.countries.model.Continent;
 import org.patrykkondrat.countries.model.Country;
 import org.patrykkondrat.countries.model.CountryDetails;
 import org.patrykkondrat.countries.service.GraphqlClientService;
 import org.patrykkondrat.countries.service.RestCountriesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.graphql.client.GraphQlClient;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,23 +20,23 @@ public class CountryController {
     @Autowired
     private RestCountriesService restCountriesService;
 
-    @PostMapping("/countries")
+    @GetMapping("/countries")
     public List<Country> getCountriesFromContinent(@RequestParam("continent") String continent) {
         return graphqlClientService.getCountriesFromContinent(continent);
     }
 
-    @PostMapping("/countries/random")
+    @GetMapping("/countries/random")
     public List<Country> getRandomCountriesFromContinent(@RequestParam("continent") String continent,
                                                          @RequestParam("numRandom") int numRandom) {
         return graphqlClientService.getRandomCountriesFromContinent(continent, numRandom);
     }
 
-    @PostMapping("/countries/details")
+    @GetMapping("/countries/details")
     public CountryDetails getCountriesDetails(@RequestParam("countryName") String countryName) {
         return restCountriesService.getCountryDetailsByCountryName(countryName);
     }
 
-    @PostMapping("/countries/details/random")
+    @GetMapping("/countries/details/random")
     public List<CountryDetails> getRandomCountryDetails(@RequestParam("continent") String continent,
                                                         @RequestParam("numRandom") int numRandom) {
         return restCountriesService.getRandomCountryDetails(continent, numRandom);
